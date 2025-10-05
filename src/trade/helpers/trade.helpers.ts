f3c6e7a0a474ac93b5c3ee428b37d27aab4b3756
Cayo Legal <cayolegal@hotmail.com>
2025-10-05 19:26:44 -0300

    force deploy

--- src/trade/helpers/trade.helpers.ts
+++ src/trade/helpers/trade.helpers.ts
@@ -12,6 +12,7 @@ export class TradeHelpers {
     };
 
     const numericCategory = Number(category);
+
     if (!isNaN(numericCategory) && numericCategory > 0) {
       where.interests = { some: { id: numericCategory } };
     }
